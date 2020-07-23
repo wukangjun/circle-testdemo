f3bce3865c471919959206dfcab17e0bdb66fb4a
吴康俊 <wukangjun@innotechx.com>
2020-07-23 18:17:32 +0800

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { 
+  BrowserRouter as Router,
+  HashRouter,
+  Link,
+  NavLink,
+  Route,
+  MemoryRouter,
+  Prompt,
+  Redirect,
+  StaticRouter,
+  Switch,
+  generatePath,
+  matchPath,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch,
+  withRouter
+ } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Manager from './pages/Manager'
+import Paint from './pages/Paint'
 
+/**
+ * @automating-route: /home
+ */
 function App() {
+  useEffect(() => {
+    window.onpopstate = function(hash: PopStateEvent) {
+      console.log(hash)
+    }
+    
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Switch>
+            <Route path="/m">
+              <Manager />
+            </Route>
+            <Route path="/p">
+              <Paint />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Paint/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Paint() {
+  return (
+    <div>paint</div>
+  );
+}
