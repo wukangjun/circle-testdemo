1e72f412c37aad8b5de615fc72cc2f8c0e030833
wukangjun <838927844@qq.com>
2020-08-19 12:11:57 +0800

    publish sh

---
+++ scripts/publish.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+set -e
+
+# 打印当前工作路径
+pwd
+remote=$(git config remote.origin.url)
+echo "remote is: ${remote}"
+
+mkdir gh-pages-branch
+cd gh-pages-branch
+
+git config --global user.email "$GH_EMAIL" >/dev/null 2>&1
+git config --global user.name "$GH_NAME" >/dev/null 2>&1
+git init
+git remote add --fetch origin "$remotet"
+
+if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
+  git checkout gh-pages
+  git rm -rf .
+else
+  git checkout --orphan gh-pages
+fi
+
+cp -a "../${siteSource}/."
+
+git add -A
+git commit --allow-empty -m "Deploy to GitHub"
+git push --force --quiet origin gh-pages
+cd ..
+rm -rf gh-pages-branch
+
+echo "Finished Deployment!"
+
+
+
+
+
+
+
+
+
+
+
